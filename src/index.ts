ab0d7a370558f502690f7d4ddd9aa134ef662bf0
Ming Tsai <ming.jia1213@gmail.com>
2021-05-26 16:08:57 -0400

    chore: use fs watcher for check file changed

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,54 @@
 import { JOB_TASKS as tasks } from './jobs/index';
-import cron from 'node-cron';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 import { JobConfiguration } from './models/job-configuration';
 import chalk from 'chalk';
 
-export default cron.schedule('* * * * *', () => {
-    try {
-        const filePath = path.resolve(__dirname, './configurations/job-configuration.yaml');
-        const fileContents: string = fs.readFileSync(filePath, 'utf8');
-        const data = yaml.load(fileContents) as Array<JobConfiguration>;
-        data.forEach((d) => {
-            const index = tasks.findIndex((t) => t.name === d.name);
-            if (index !== -1) {
-                if (d.isEnabled && d.schedule !== tasks[index].schedule) {
-                    tasks[index].stop();
-                    tasks[index].setSchedule(d.schedule);
-                }
-                
-                if (d.isEnabled && !tasks[index].isStarted()) {
-                    console.log(chalk.green(`The task ${tasks[index].name} has started`));
-                    tasks[index].start();
-                }
-
-                if (!d.isEnabled && tasks[index].isStarted()) {
-                    console.log(chalk.green(`The task ${tasks[index].name} has stopped`));
-                    tasks[index].stop();
-                }
+let watcher: fs.FSWatcher;
+function main() {
+    const filePath = path.resolve(__dirname, './configurations/job-configuration.yaml');
+    startJobs(filePath);
+    watcher = fs.watch(filePath, (event, __) => {
+        console.log(chalk.gray('File has been modified'));
+        try {
+            if (event === 'change') {
+                startJobs(filePath);
+            } else {
+                closeWatcher();
             }
-        });
-    } catch (error) {
-        console.log(chalk.red(error));
-    }
-});
+        } catch (error) {
+            console.log(chalk.red(error));
+        }
+    });
+}
+
+const startJobs = (filePath: string): void => {
+    const fileContents: string = fs.readFileSync(filePath, 'utf8');
+    const data = yaml.load(fileContents) as Array<JobConfiguration>;
+    data.forEach((d) => {
+        const index = tasks.findIndex((t) => t.name === d.name);
+        if (index !== -1) {
+            if (d.isEnabled && d.schedule !== tasks[index].schedule) {
+                tasks[index].stop();
+                tasks[index].setSchedule(d.schedule);
+            }
+
+            if (d.isEnabled && !tasks[index].isStarted()) {
+                console.log(chalk.green(`The task ${tasks[index].name} has started`));
+                tasks[index].start();
+            }
+
+            if (!d.isEnabled && tasks[index].isStarted()) {
+                console.log(chalk.green(`The task ${tasks[index].name} has stopped`));
+                tasks[index].stop();
+            }
+        }
+    });
+}
+
+const closeWatcher = (): void => {
+    watcher.close();
+}
+
+main();
